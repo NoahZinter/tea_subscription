ea734a917e18c1bc090e8cea7f8a96337aca4172
NoahZinter <noah.zinter@gmail.com>
2021-09-14 11:16:13 -0600

    Model test subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,6 @@
 class Subscription < ApplicationRecord
-  belongs_to :customers
+  belongs_to :customer
+
+  enum status: { 'pending': 0, 'cancelled': 1, 'active': 2 }
+  enum frequency: { 'weekly': 0, 'monthly': 1, 'bi-monthly': 2 }
 end
