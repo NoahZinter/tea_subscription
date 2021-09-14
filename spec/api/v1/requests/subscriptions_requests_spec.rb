fc86be5abd28b1ddb1d663029fced22587ed3bfc
NoahZinter <noah.zinter@gmail.com>
2021-09-14 12:59:49 -0600

    Edit teas table

--- spec/api/v1/requests/subscriptions_requests_spec.rb
+++ spec/api/v1/requests/subscriptions_requests_spec.rb
@@ -2,7 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Subscription Requests' do
   describe 'create' do
-    it 'creates a new subscription'
+    before(:all) do
+      @customer = create(:customer)
+      @teas = create_list(:tea, 4)
+      @subscription = @customer.subscriptions.create!(price: 10.5)
+      @teas.each do |tea|
+        SubscriptionTea.create!(tea_id: tea.id, subscription_id: @subscription.id)
+      end
+    end
+
+    it 'creates a new subscription' do
+      subscription_params = {
+        price: 10,
+        frequency: 1,
+        teas: @teas
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json'}
+      post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers, params: JSON.generate(subscription_params)
+
+      # expect(response.status).to eq 201
+
+      # subscription = Subscription.last
+
+      binding.pry
+    end
 
     it 'returns serialized json data'
 
--- spec/factories/tea.rb
+++ spec/factories/tea.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tea do
-    type { Faker::Tea.variety }
+    variety { Faker::Tea.variety }
     description { Faker::Emotion.noun }
     temperature { Faker::Number.between(from: 70, to: 110) }
     brew_time { Faker::Number.between(from: 1, to: 6) }
