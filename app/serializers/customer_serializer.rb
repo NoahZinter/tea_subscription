4849d6cd73b9d0325ef8187c11ff86b03a113f4b
NoahZinter <noah.zinter@gmail.com>
2021-09-14 13:55:35 -0600

    Test subscription creation

---
+++ app/serializers/customer_serializer.rb
@@ -0,0 +1,5 @@
+class CustomerSerializer
+  include JSONAPI::Serializer
+  attributes :first_name, :last_name, :address, :email
+  has_many :subscriptions
+end
--- spec/api/v1/requests/subscriptions_requests_spec.rb
+++ spec/api/v1/requests/subscriptions_requests_spec.rb
@@ -4,30 +4,53 @@ RSpec.describe 'Subscription Requests' do
   describe 'create' do
     before(:all) do
       @customer = create(:customer)
-      @teas = create_list(:tea, 4)
-      @subscription = @customer.subscriptions.create!(price: 10.5)
-      @teas.each do |tea|
-        SubscriptionTea.create!(tea_id: tea.id, subscription_id: @subscription.id)
-      end
     end
 
     it 'creates a new subscription' do
       subscription_params = {
-        price: 10,
         frequency: 1,
-        teas: @teas
+        price: 10.5
       }
       headers = { 'CONTENT_TYPE' => 'application/json'}
       post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers, params: JSON.generate(subscription_params)
 
-      # expect(response.status).to eq 201
+      expect(response.status).to eq 201
 
-      # subscription = Subscription.last
-
-      binding.pry
+      subscription = Subscription.last
+      expect(subscription.status).to eq 'pending'
+      expect(subscription.title).to eq 'untitled subscription'
+      expect(subscription.price).to eq 10.5
+      expect(subscription.frequency).to eq 'monthly'
+      expect(subscription.customer_id).to eq @customer.id
     end
 
-    it 'returns serialized json data'
+    it 'returns serialized json data' do
+      subscription_params = {
+        frequency: 1,
+        price: 10.5
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json'}
+      post "/api/v1/customers/#{@customer.id}/subscriptions", headers: headers, params: JSON.generate(subscription_params)
+      expect(response.status).to eq 201
+      subscription = JSON.parse(response.body, symbolize_names: true)
+
+      expect(subscription).is_a? Hash
+      expect(subscription).to have_key(:data)
+      expect(subscription[:data]).is_a? Hash
+      expect(subscription[:data]).to have_key(:id)
+      expect(subscription[:data][:id]).is_a? String
+      expect(subscription[:data]).to have_key(:type)
+      expect(subscription[:data][:type]).to eq 'subscription'
+      expect(subscription[:data]).to have_key(:attributes)
+      expect(subscription[:data][:attributes]).is_a? Hash
+      expect(subscription[:data][:attributes]).to have_key(:title)
+      expect(subscription[:data][:attributes][:title]).to eq 'untitled subscription'
+      expect(subscription[:data][:attributes]).to have_key(:price)
+      expect(subscription[:data][:attributes][:price]).to eq 10.5
+      expect(subscription[:data][:attributes]).to have_key(:frequency)
+      expect(subscription[:data][:attributes][:frequency]).to eq 'monthly'
+
+    end
 
     it 'ignores extra attributes'
 
