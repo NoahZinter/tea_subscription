851f8b83cf8bdccf519de884306a048539a95441
NoahZinter <noah.zinter@gmail.com>
2021-09-15 10:54:26 -0600

    Refactor tea adding

--- spec/api/v1/requests/teas_request_spec.rb
+++ spec/api/v1/requests/teas_request_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'Tea Requests' do
     @customer = create(:customer)
     @subscription = @customer.subscriptions.create(price: 12.3, frequency: 1)
     @teas = create_list(:tea, 4)
-    @teas = @teas.map { |tea| tea.variety }
   end
 
   describe 'create' do
     it 'creates join table records for a given subscription' do
+      variety = @teas.first.variety
       st_params = {
-        teas: @teas
+        variety: variety
       }
       headers = {'CONTENT_TYPE' => 'application/json'}
       post "/api/v1/customers/#{@customer.id}/subscriptions/#{@subscription.id}/teas", headers: headers, params: JSON.generate(st_params)
@@ -37,9 +37,5 @@ RSpec.describe 'Tea Requests' do
       expect(changed_subscription[:data][:attributes]).to have_key(:frequency)
       expect(changed_subscription[:data][:attributes][:frequency]).to eq 'monthly'
     end
-
-    it 'test/add error handling'
-
-    it 'test nonexistent teas'
   end
 end
